58d7f5f37842525a7b61d017feef2cadafdf56bd
Jérôme Wiedemann <jerome@wnetworks.org>
2021-01-25 23:13:59 +0000

    chore(changelog): Add doc and chores sections

--- release.config.js
+++ release.config.js
@@ -2,30 +2,47 @@
 module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        presetConfig: {
+          types: [
+            { type: 'feat', section: 'Features' },
+            { type: 'fix', section: 'Bug Fixes' },
+            { type: 'doc', hidden: false, section: 'Documentation' },
+            { type: 'docs', hidden: false, section: 'Documentation' },
+            { type: 'chore', hidden: false, section: 'Chores' },
+          ],
+        },
+      },
+    ],
     '@semantic-release/changelog',
-    ['@semantic-release/npm', {
-      npmPublish: false,
-    }],
-    ['@semantic-release/exec', {
-      prepareCmd: './scripts/update_readme.sh "${nextRelease.version}" "$GITHUB_REF"',
-    }],
-    ['@semantic-release/git', {
-      assets: [
-        'CHANGELOG.md',
-        'README.md',
-        'package.json',
-        'package-lock.json',
-        'npm-shrinkwrap.json',
-      ],
-    }],
-    ['@semantic-release/github', {
-      assets: 'dist/*.js',
-    }],
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: './scripts/update_readme.sh "${nextRelease.version}" "$GITHUB_REF"',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'README.md', 'package.json', 'package-lock.json', 'npm-shrinkwrap.json'],
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: 'dist/*.js',
+      },
+    ],
   ],
   preset: 'angular',
-  branches: [
-    { name: 'master' },
-    { name: 'dev', prerelease: true },
-  ],
+  branches: [{ name: 'master' }, { name: 'dev', prerelease: true }],
 };
