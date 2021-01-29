RomRider/apexcharts-card

commit af8ba8156452054c38b4b65bb843bb16fbfdf6bd

feat(series.offset): Compare data from another timeframe with the current timeframe (#19)
Fixes #18

src/apexcharts-card.ts
@@ -95,6 +95,8 @@ class ChartsCard extends LitElement {
 
   private _dataLoaded = false;
 
+  private _seriesOffset: number[] = [];
+
   public connectedCallback() {
     super.connectedCallback();
     if (this._config && this._hass && !this._loaded) {
@@ -194,6 +196,11 @@ class ChartsCard extends LitElement {
     if (config.span?.end && config.span?.start) {
       throw new Error(`span: Only one of 'start' or 'end' is allowed.`);
     }
+    config.series.forEach((serie, index) => {
+      if (serie.offset) {
+        this._seriesOffset[index] = validateOffset(serie.offset, `series[${index}].offset`);
+      }
+    });
 
     this._config = mergeDeep(
       {
@@ -231,6 +238,7 @@ class ChartsCard extends LitElement {
             this._config!.cache,
             serie,
             this._config?.span,
+            this._seriesOffset[index] || 0,
           );
         }
         return undefined;

src/graphEntry.ts
@@ -1,7 +1,7 @@
 import { HomeAssistant } from 'custom-card-helpers';
 import { ChartCardSeriesConfig, EntityCachePoints, EntityEntryCache, HassHistory, HistoryBuckets } from './types';
 import { compress, decompress, log } from './utils';
-import localForage from 'localforage';
+import localForage, { config } from 'localforage';
 import { HassEntity } from 'home-assistant-js-websocket';
 import { DateRange } from 'moment-range';
 import { HOUR_24, moment } from './const';
@@ -47,12 +47,15 @@ export default class GraphEntry {
 
   private _md5Config: string;
 
+  private _offset = 0;
+
   constructor(
     index: number,
     graphSpan: number,
     cache: boolean,
     config: ChartCardSeriesConfig,
     span: ChartCardSpanExtConfig | undefined,
+    offset = 0,
   ) {
     const aggregateFuncMap = {
       avg: this._average,
@@ -70,6 +73,7 @@ export default class GraphEntry {
     this._history = undefined;
     this._graphSpan = graphSpan;
     this._config = config;
+    this._offset = offset;
     const now = new Date();
     const now2 = new Date(now);
     this._func = aggregateFuncMap[config.group_by.func];
@@ -89,7 +93,7 @@ export default class GraphEntry {
   }
 
   get history(): EntityCachePoints {
-    return this._computedHistory || this._history?.data || [];
+    return this._offsetData(this._computedHistory || this._history?.data || []);
   }
 
   get index(): number {
@@ -102,6 +106,17 @@ export default class GraphEntry {
 
   get end(): Date {
     return this._realEnd;
+  }
+
+  private _offsetData(data: EntityCachePoints) {
+    if (this._offset) {
+      const lData = JSON.parse(JSON.stringify(data));
+      lData.forEach((entry) => {
+        entry[0] = entry[0] - this._offset;
+      });
+      return lData;
+    }
+    return data;
   }
 
   private async _getCache(key: string, compressed: boolean): Promise<EntityEntryCache | undefined> {
@@ -136,8 +151,14 @@ export default class GraphEntry {
     if (this._config.data_generator) {
       this._history = this._generateData(start, end);
     } else {
-      this._realStart = start;
-      this._realEnd = end;
+      this._realStart = new Date(start);
+      this._realEnd = new Date(end);
+      const endHistory = new Date(end);
+
+      if (this._offset) {
+        startHistory.setTime(startHistory.getTime() + this._offset);
+        endHistory.setTime(endHistory.getTime() + this._offset);
+      }
 
       let skipInitialState = false;
 
@@ -165,7 +186,7 @@ export default class GraphEntry {
           ? // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
             new Date(history.data.slice(-1)[0]![0] + 1)
           : startHistory,
-        end,
+        endHistory,
         skipInitialState,
       );
       if (newHistory && newHistory[0] && newHistory[0].length > 0) {

src/utils.ts
@@ -63,17 +63,17 @@ export function computeName(
   entity: HassEntity | undefined = undefined,
 ): string {
   if (!config || (!entities && !entity)) return '';
+  let name = '';
   if (entity) {
-    return config.series[index].name || entity.attributes?.friendly_name || entity.entity_id || '';
+    name = config.series[index].name || entity.attributes?.friendly_name || entity.entity_id || '';
   } else if (entities) {
-    return (
+    name =
       config.series[index].name ||
       entities[index]?.attributes?.friendly_name ||
       entities[entities[index]]?.entity_id ||
-      ''
-    );
+      '';
   }
-  return '';
+  return name + (config.series[index].offset ? ` (${config.series[index].offset})` : '');
 }
 
 export function computeUom(

.devcontainer/ui-lovelace.yaml
@@ -25,6 +25,7 @@ views:
           - entity: sensor.random_0_1000
             name: Sensor 2
             type: area
+            offset: -1d
         graph_span: 15min
         cache: true
         layout: minimal
@@ -33,6 +34,8 @@ views:
 
       - type: custom:apexcharts-card
         stacked: true
+        span:
+          offset: -1d
         series:
           - entity: sensor.random_0_1000
             name: RAM Usage
@@ -228,3 +231,22 @@ views:
                 const value = entity.attributes[attr];
                 return [moment().startOf('day').hours(hour).valueOf(), value];
               })
+
+      - type: custom:apexcharts-card
+        graph_span: 8d
+        span:
+          start: hour
+        # apex_config:
+        #   dataLabels:
+        #     enabled: true
+        header:
+          show: true
+          title: Precipitation Forecast
+        series:
+          - entity: weather.openweathermap
+            type: area
+            extend_to_end: false
+            data_generator: |
+              return entity.attributes.forecast.map((entry) => {
+                return [new Date(entry.datetime), entry.temperature];
+              });

src/types-config-ti.ts
@@ -39,6 +39,7 @@ export const ChartCardSeriesExternalConfig = t.iface([], {
   "invert": t.opt("boolean"),
   "data_generator": t.opt("string"),
   "float_precision": t.opt("number"),
+  "offset": t.opt("string"),
   "group_by": t.opt(t.iface([], {
     "duration": t.opt("string"),
     "func": t.opt("GroupByFunc"),

src/types-config.ts
@@ -35,6 +35,7 @@ export interface ChartCardSeriesExternalConfig {
   invert?: boolean;
   data_generator?: string;
   float_precision?: number;
+  offset?: string;
   group_by?: {
     duration?: string;
     func?: GroupByFunc;

commit f5c0d57f17c2e799d2890b78b12b9a7dd685703f

fix(series.offset): Fix bug introduced by #19
Fixes #18

src/apexcharts-card.ts
@@ -1,6 +1,6 @@
 import { LitElement, html, customElement, property, TemplateResult, CSSResult, PropertyValues } from 'lit-element';
 import { ClassInfo, classMap } from 'lit-html/directives/class-map';
-import { ChartCardConfig, EntityEntryCache } from './types';
+import { ChartCardConfig, EntityCachePoints, EntityEntryCache } from './types';
 import { HomeAssistant } from 'custom-card-helpers';
 import localForage from 'localforage';
 import * as pjson from '../package.json';
@@ -11,6 +11,7 @@ import {
   decompress,
   log,
   mergeDeep,
+  offsetData,
   validateInterval,
   validateOffset,
 } from './utils';
@@ -238,7 +239,6 @@ class ChartsCard extends LitElement {
             this._config!.cache,
             serie,
             this._config?.span,
-            this._seriesOffset[index] || 0,
           );
         }
         return undefined;
@@ -352,7 +352,12 @@ class ChartsCard extends LitElement {
 
     const { start, end } = this._getSpanDates();
     try {
-      const promise = this._graphs.map((graph) => graph?._updateHistory(start, end));
+      const promise = this._graphs.map((graph, index) =>
+        graph?._updateHistory(
+          this._seriesOffset[index] ? new Date(start.getTime() + this._seriesOffset[index]) : start,
+          this._seriesOffset[index] ? new Date(end.getTime() + this._seriesOffset[index]) : end,
+        ),
+      );
       await Promise.all(promise);
       const graphData = {
         series: this._graphs.map((graph) => {
@@ -373,13 +378,14 @@ class ChartsCard extends LitElement {
               this._lastState[index] = (this._lastState[index] as number).toFixed(precision);
             }
           }
-          let data: (number | null)[][] = [];
+          let data: EntityCachePoints = [];
           if (this._config?.series[index].extend_to_end && this._config?.series[index].type !== 'column') {
             // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            data = [...graph.history, ...[[end.getTime(), graph.history.slice(-1)[0]![1]]]];
+            data = [...graph.history, ...([[end.getTime(), graph.history.slice(-1)[0]![1]]] as EntityCachePoints)];
           } else {
             data = graph.history;
           }
+          data = offsetData(data, this._seriesOffset[index]);
           return this._config?.series[index].invert ? { data: this._invertData(data) } : { data };
         }),
         xaxis: {
@@ -419,7 +425,7 @@ class ChartsCard extends LitElement {
     return new Date(localEnd.getTime() - (offsetEnd ? offsetEnd : 0)).getTime();
   }
 
-  private _invertData(data: (number | null)[][]): (number | null)[][] {
+  private _invertData(data: EntityCachePoints): EntityCachePoints {
     return data.map((item) => {
       if (item[1] === null) return item;
       return [item[0], -item[1]];

src/graphEntry.ts
@@ -1,7 +1,7 @@
 import { HomeAssistant } from 'custom-card-helpers';
 import { ChartCardSeriesConfig, EntityCachePoints, EntityEntryCache, HassHistory, HistoryBuckets } from './types';
 import { compress, decompress, log } from './utils';
-import localForage, { config } from 'localforage';
+import localForage from 'localforage';
 import { HassEntity } from 'home-assistant-js-websocket';
 import { DateRange } from 'moment-range';
 import { HOUR_24, moment } from './const';
@@ -47,15 +47,12 @@ export default class GraphEntry {
 
   private _md5Config: string;
 
-  private _offset = 0;
-
   constructor(
     index: number,
     graphSpan: number,
     cache: boolean,
     config: ChartCardSeriesConfig,
     span: ChartCardSpanExtConfig | undefined,
-    offset = 0,
   ) {
     const aggregateFuncMap = {
       avg: this._average,
@@ -73,7 +70,6 @@ export default class GraphEntry {
     this._history = undefined;
     this._graphSpan = graphSpan;
     this._config = config;
-    this._offset = offset;
     const now = new Date();
     const now2 = new Date(now);
     this._func = aggregateFuncMap[config.group_by.func];
@@ -93,7 +89,7 @@ export default class GraphEntry {
   }
 
   get history(): EntityCachePoints {
-    return this._offsetData(this._computedHistory || this._history?.data || []);
+    return this._computedHistory || this._history?.data || [];
   }
 
   get index(): number {
@@ -106,17 +102,6 @@ export default class GraphEntry {
 
   get end(): Date {
     return this._realEnd;
-  }
-
-  private _offsetData(data: EntityCachePoints) {
-    if (this._offset) {
-      const lData = JSON.parse(JSON.stringify(data));
-      lData.forEach((entry) => {
-        entry[0] = entry[0] - this._offset;
-      });
-      return lData;
-    }
-    return data;
   }
 
   private async _getCache(key: string, compressed: boolean): Promise<EntityEntryCache | undefined> {
@@ -153,12 +138,6 @@ export default class GraphEntry {
     } else {
       this._realStart = new Date(start);
       this._realEnd = new Date(end);
-      const endHistory = new Date(end);
-
-      if (this._offset) {
-        startHistory.setTime(startHistory.getTime() + this._offset);
-        endHistory.setTime(endHistory.getTime() + this._offset);
-      }
 
       let skipInitialState = false;
 
@@ -186,7 +165,7 @@ export default class GraphEntry {
           ? // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
             new Date(history.data.slice(-1)[0]![0] + 1)
           : startHistory,
-        endHistory,
+        end,
         skipInitialState,
       );
       if (newHistory && newHistory[0] && newHistory[0].length > 0) {

src/utils.ts
@@ -1,6 +1,6 @@
 import { HassEntities, HassEntity } from 'home-assistant-js-websocket';
 import { compress as lzStringCompress, decompress as lzStringDecompress } from 'lz-string';
-import { ChartCardConfig } from './types';
+import { ChartCardConfig, EntityCachePoints } from './types';
 import { TinyColor } from '@ctrl/tinycolor';
 import parse from 'parse-duration';
 
@@ -123,4 +123,15 @@ export function validateOffset(interval: string, prefix: string): number {
     throw new Error(`'${prefix}: ${interval}' should start with a '+' or a '-'`);
   }
   return validateInterval(interval, prefix);
+}
+
+export function offsetData(data: EntityCachePoints, offset: number | undefined): EntityCachePoints {
+  if (offset) {
+    const lData = JSON.parse(JSON.stringify(data));
+    lData.forEach((entry) => {
+      entry[0] = entry[0] - offset;
+    });
+    return lData;
+  }
+  return data;
 }