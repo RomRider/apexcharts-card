730f236df1c0e406288d95060950a803d0102fd4
Jérôme Wiedemann <jerome@wnetworks.org>
2021-02-04 21:27:01 +0000

    chore: Refactor ApexCharts' config generation

--- src/apex-layouts.ts
+++ src/apex-layouts.ts
@@ -3,13 +3,13 @@ import parse from 'parse-duration';
 import { DEFAULT_FLOAT_PRECISION, DEFAULT_SERIE_TYPE, HOUR_24, moment, NO_VALUE, TIMESERIES_TYPES } from './const';
 import { ChartCardConfig } from './types';
 import { computeName, computeUom, mergeDeep, prettyPrintTime } from './utils';
+import { layoutMinimal } from './layouts/minimal';
 
 export function getLayoutConfig(config: ChartCardConfig, hass: HomeAssistant | undefined = undefined): unknown {
   const def = {
     chart: {
       type: config.chart_type || DEFAULT_SERIE_TYPE,
       stacked: config?.stacked,
-      // type: 'line',
       foreColor: 'var(--primary-text-color)',
       width: '100%',
       zoom: {
@@ -23,156 +23,33 @@ export function getLayoutConfig(config: ChartCardConfig, hass: HomeAssistant | u
       strokeDashArray: 3,
     },
     fill: {
-      opacity: config.series.flatMap((serie) => {
-        if (!serie.show.in_chart) return [];
-        return [serie.type === 'area' ? 0.7 : 1];
-      }),
+      opacity: getFillOpacity(config),
     },
-    series: TIMESERIES_TYPES.includes(config.chart_type)
-      ? config?.series.flatMap((serie, index) => {
-          if (!serie.show.in_chart) return [];
-          return [
-            {
-              name: computeName(index, config.series, undefined, hass?.states[serie.entity]),
-              type: serie.type,
-              data: [],
-            },
-          ];
-        })
-      : [],
-    labels: TIMESERIES_TYPES.includes(config.chart_type)
-      ? []
-      : config.series.flatMap((serie, index) => {
-          if (!serie.show.in_chart) return [];
-          return [computeName(index, config.series, undefined, hass?.states[serie.entity])];
-        }),
-    xaxis: TIMESERIES_TYPES.includes(config.chart_type)
-      ? {
-          type: 'datetime',
-          // range: getMilli(config.hours_to_show),
-          labels: {
-            datetimeUTC: false,
-            datetimeFormatter: getDateTimeFormatter(config.hours_12),
-          },
-        }
-      : {},
-    yaxis: Array.isArray(config.apex_config?.yaxis)
-      ? undefined
-      : {
-          decimalsInFloat: config.y_axis_precision === undefined ? DEFAULT_FLOAT_PRECISION : config.y_axis_precision,
-        },
+    series: getSeries(config, hass),
+    labels: getLabels(config, hass),
+    xaxis: getXAxis(config),
+    yaxis: getYAxis(config),
     tooltip: {
       x: {
         formatter: getXTooltipFormatter(config),
       },
       y: {
-        formatter: function (value, opts, conf = config, hass2 = hass) {
-          if (
-            value !== null &&
-            typeof value === 'number' &&
-            !Number.isInteger(value) &&
-            !conf.series_in_graph[opts.seriesIndex]?.show.as_duration
-          ) {
-            value = (value as number).toFixed(
-              conf.series_in_graph[opts.seriesIndex].float_precision === undefined
-                ? DEFAULT_FLOAT_PRECISION
-                : conf.series_in_graph[opts.seriesIndex].float_precision,
-            );
-          }
-          const uom = computeUom(
-            opts.seriesIndex,
-            conf.series_in_graph,
-            undefined,
-            hass2?.states[conf.series_in_graph[opts.seriesIndex].entity],
-          );
-          return conf.series_in_graph[opts.seriesIndex]?.show.as_duration
-            ? // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-              [`<strong>${prettyPrintTime(value, conf.series_in_graph[opts.seriesIndex].show.as_duration!)}</strong>`]
-            : [`<strong>${value} ${uom}</strong>`];
-        },
+        formatter: getYTooltipFormatter(config, hass),
       },
     },
     dataLabels: {
-      formatter: function (value, opts, conf = config) {
-        if (value === null) return;
-        let lValue = value;
-        if (lValue !== null && typeof lValue === 'number' && !Number.isInteger(lValue)) {
-          lValue = (lValue as number).toFixed(
-            conf.series_in_graph[opts.seriesIndex].float_precision === undefined
-              ? DEFAULT_FLOAT_PRECISION
-              : conf.series_in_graph[opts.seriesIndex].float_precision,
-          );
-        }
-        return lValue;
-      },
+      formatter: getDataLabelsFormatter(config),
     },
     plotOptions: {
-      radialBar:
-        config.chart_type === 'radialBar'
-          ? {
-              track: {
-                background: 'rgba(128, 128, 128, 0.2)',
-              },
-            }
-          : {},
+      radialBar: getPlotOptions_radialBar(config),
     },
     legend: {
       position: 'bottom',
       show: true,
-      formatter: function (_, opts, conf = config, hass2 = hass) {
-        const name = computeName(
-          opts.seriesIndex,
-          conf.series_in_graph,
-          undefined,
-          hass2?.states[conf.series_in_graph[opts.seriesIndex].entity],
-        );
-        if (!conf.series_in_graph[opts.seriesIndex].show.legend_value) {
-          return [name];
-        } else {
-          let value = TIMESERIES_TYPES.includes(config.chart_type)
-            ? opts.w.globals.series[opts.seriesIndex].slice(-1)[0]
-            : opts.w.globals.series[opts.seriesIndex];
-          if (
-            value !== null &&
-            typeof value === 'number' &&
-            !Number.isInteger(value) &&
-            !conf.series_in_graph[opts.seriesIndex]?.show.as_duration
-          ) {
-            value = (value as number).toFixed(
-              conf.series_in_graph[opts.seriesIndex].float_precision === undefined
-                ? DEFAULT_FLOAT_PRECISION
-                : conf.series_in_graph[opts.seriesIndex].float_precision,
-            );
-          }
-          const uom = computeUom(
-            opts.seriesIndex,
-            conf.series_in_graph,
-            undefined,
-            hass2?.states[conf.series_in_graph[opts.seriesIndex].entity],
-          );
-          let valueString = '';
-          if (value === undefined || value === null) {
-            valueString = `<strong>${NO_VALUE} ${uom}</strong>`;
-          } else {
-            if (conf.series_in_graph[opts.seriesIndex]?.show.as_duration) {
-              valueString = `<strong>${prettyPrintTime(
-                value,
-                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                conf.series_in_graph[opts.seriesIndex].show.as_duration!,
-              )}</strong>`;
-            } else {
-              valueString = `<strong>${value} ${uom}</strong>`;
-            }
-          }
-          return [name + ':', valueString];
-        }
-      },
+      formatter: getLegendFormatter(config, hass),
     },
     stroke: {
-      curve: config.series.flatMap((serie) => {
-        if (!serie.show.in_chart) return [];
-        return [serie.curve || 'smooth'];
-      }),
+      curve: getStrokeCurve(config),
       lineCap: config.chart_type === 'radialBar' ? 'round' : 'butt',
       colors:
         config.chart_type === 'pie' || config.chart_type === 'donut' ? ['var(--card-background-color)'] : undefined,
@@ -188,46 +65,7 @@ export function getLayoutConfig(config: ChartCardConfig, hass: HomeAssistant | u
   let conf = {};
   switch (config.layout) {
     case 'minimal':
-      conf = {
-        chart: {
-          offsetY: 15,
-          parentHeightOffset: 0,
-        },
-        grid: {
-          show: false,
-          padding: {
-            left: 0,
-            right: 0,
-          },
-        },
-        xaxis: {
-          labels: {
-            show: false,
-          },
-          axisBorder: {
-            show: false,
-          },
-          axisTicks: {
-            show: false,
-          },
-          crosshairs: {
-            show: true,
-          },
-          tooltip: {
-            enabled: false,
-          },
-        },
-        yaxis: {
-          show: false,
-          showAlways: true,
-          tooltip: {
-            enabled: true,
-          },
-        },
-        legend: {
-          position: 'top',
-        },
-      };
+      conf = layoutMinimal;
       break;
 
     default:
@@ -235,6 +73,64 @@ export function getLayoutConfig(config: ChartCardConfig, hass: HomeAssistant | u
   }
 
   return config.apex_config ? mergeDeep(mergeDeep(def, conf), config.apex_config) : mergeDeep(def, conf);
+}
+
+function getFillOpacity(config: ChartCardConfig): number[] {
+  return config.series.flatMap((serie) => {
+    if (!serie.show.in_chart) return [];
+    return [serie.type === 'area' ? 0.7 : 1];
+  });
+}
+
+function getSeries(config: ChartCardConfig, hass: HomeAssistant | undefined) {
+  if (TIMESERIES_TYPES.includes(config.chart_type)) {
+    return config?.series.flatMap((serie, index) => {
+      if (!serie.show.in_chart) return [];
+      return [
+        {
+          name: computeName(index, config.series, undefined, hass?.states[serie.entity]),
+          type: serie.type,
+          data: [],
+        },
+      ];
+    });
+  } else {
+    return [];
+  }
+}
+
+function getLabels(config: ChartCardConfig, hass: HomeAssistant | undefined) {
+  if (TIMESERIES_TYPES.includes(config.chart_type)) {
+    return [];
+  } else {
+    return config.series.flatMap((serie, index) => {
+      if (!serie.show.in_chart) return [];
+      return [computeName(index, config.series, undefined, hass?.states[serie.entity])];
+    });
+  }
+}
+
+function getXAxis(config: ChartCardConfig) {
+  if (TIMESERIES_TYPES.includes(config.chart_type)) {
+    return {
+      type: 'datetime',
+      // range: getMilli(config.hours_to_show),
+      labels: {
+        datetimeUTC: false,
+        datetimeFormatter: getDateTimeFormatter(config.hours_12),
+      },
+    };
+  } else {
+    return {};
+  }
+}
+
+function getYAxis(config: ChartCardConfig) {
+  return Array.isArray(config.apex_config?.yaxis)
+    ? undefined
+    : {
+        decimalsInFloat: config.y_axis_precision === undefined ? DEFAULT_FLOAT_PRECISION : config.y_axis_precision,
+      };
 }
 
 function getDateTimeFormatter(hours12: boolean | undefined): unknown {
@@ -273,4 +169,116 @@ function getXTooltipFormatter(config: ChartCardConfig): ((val: number) => string
     : function (val) {
         return moment(new Date(val)).format(days);
       };
+}
+
+function getYTooltipFormatter(config: ChartCardConfig, hass: HomeAssistant | undefined) {
+  return function (value, opts, conf = config, hass2 = hass) {
+    if (
+      value !== null &&
+      typeof value === 'number' &&
+      !Number.isInteger(value) &&
+      !conf.series_in_graph[opts.seriesIndex]?.show.as_duration
+    ) {
+      value = (value as number).toFixed(
+        conf.series_in_graph[opts.seriesIndex].float_precision === undefined
+          ? DEFAULT_FLOAT_PRECISION
+          : conf.series_in_graph[opts.seriesIndex].float_precision,
+      );
+    }
+    const uom = computeUom(
+      opts.seriesIndex,
+      conf.series_in_graph,
+      undefined,
+      hass2?.states[conf.series_in_graph[opts.seriesIndex].entity],
+    );
+    return conf.series_in_graph[opts.seriesIndex]?.show.as_duration
+      ? // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        [`<strong>${prettyPrintTime(value, conf.series_in_graph[opts.seriesIndex].show.as_duration!)}</strong>`]
+      : [`<strong>${value} ${uom}</strong>`];
+  };
+}
+
+function getDataLabelsFormatter(config: ChartCardConfig) {
+  return function (value, opts, conf = config) {
+    if (value === null) return;
+    let lValue = value;
+    if (lValue !== null && typeof lValue === 'number' && !Number.isInteger(lValue)) {
+      lValue = (lValue as number).toFixed(
+        conf.series_in_graph[opts.seriesIndex].float_precision === undefined
+          ? DEFAULT_FLOAT_PRECISION
+          : conf.series_in_graph[opts.seriesIndex].float_precision,
+      );
+    }
+    return lValue;
+  };
+}
+
+function getPlotOptions_radialBar(config: ChartCardConfig) {
+  if (config.chart_type === 'radialBar') {
+    return {
+      track: {
+        background: 'rgba(128, 128, 128, 0.2)',
+      },
+    };
+  } else {
+    return {};
+  }
+}
+
+function getLegendFormatter(config: ChartCardConfig, hass: HomeAssistant | undefined) {
+  return function (_, opts, conf = config, hass2 = hass) {
+    const name = computeName(
+      opts.seriesIndex,
+      conf.series_in_graph,
+      undefined,
+      hass2?.states[conf.series_in_graph[opts.seriesIndex].entity],
+    );
+    if (!conf.series_in_graph[opts.seriesIndex].show.legend_value) {
+      return [name];
+    } else {
+      let value = TIMESERIES_TYPES.includes(config.chart_type)
+        ? opts.w.globals.series[opts.seriesIndex].slice(-1)[0]
+        : opts.w.globals.series[opts.seriesIndex];
+      if (
+        value !== null &&
+        typeof value === 'number' &&
+        !Number.isInteger(value) &&
+        !conf.series_in_graph[opts.seriesIndex]?.show.as_duration
+      ) {
+        value = (value as number).toFixed(
+          conf.series_in_graph[opts.seriesIndex].float_precision === undefined
+            ? DEFAULT_FLOAT_PRECISION
+            : conf.series_in_graph[opts.seriesIndex].float_precision,
+        );
+      }
+      const uom = computeUom(
+        opts.seriesIndex,
+        conf.series_in_graph,
+        undefined,
+        hass2?.states[conf.series_in_graph[opts.seriesIndex].entity],
+      );
+      let valueString = '';
+      if (value === undefined || value === null) {
+        valueString = `<strong>${NO_VALUE} ${uom}</strong>`;
+      } else {
+        if (conf.series_in_graph[opts.seriesIndex]?.show.as_duration) {
+          valueString = `<strong>${prettyPrintTime(
+            value,
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            conf.series_in_graph[opts.seriesIndex].show.as_duration!,
+          )}</strong>`;
+        } else {
+          valueString = `<strong>${value} ${uom}</strong>`;
+        }
+      }
+      return [name + ':', valueString];
+    }
+  };
+}
+
+function getStrokeCurve(config: ChartCardConfig) {
+  return config.series.flatMap((serie) => {
+    if (!serie.show.in_chart) return [];
+    return [serie.curve || 'smooth'];
+  });
 }
---
+++ src/layouts/minimal.ts
@@ -0,0 +1,40 @@
+export const layoutMinimal = {
+  chart: {
+    offsetY: 15,
+    parentHeightOffset: 0,
+  },
+  grid: {
+    show: false,
+    padding: {
+      left: 0,
+      right: 0,
+    },
+  },
+  xaxis: {
+    labels: {
+      show: false,
+    },
+    axisBorder: {
+      show: false,
+    },
+    axisTicks: {
+      show: false,
+    },
+    crosshairs: {
+      show: true,
+    },
+    tooltip: {
+      enabled: false,
+    },
+  },
+  yaxis: {
+    show: false,
+    showAlways: true,
+    tooltip: {
+      enabled: true,
+    },
+  },
+  legend: {
+    position: 'top',
+  },
+};
