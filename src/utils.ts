e1aaf691aee8bb4e8c8b75eb3fb301d1c31d20b6
Jérôme Wiedemann <jerome@wnetworks.org>
2023-04-02 15:47:46 +0000

    fix: card broken with HA 2023.4.0bXX and above
    Fix #527

--- src/utils.ts
+++ src/utils.ts
@@ -159,7 +159,7 @@ export function getLovelace(): LovelaceConfig | null {
   root = root && root.shadowRoot;
   root = root && root.querySelector('home-assistant-main');
   root = root && root.shadowRoot;
-  root = root && root.querySelector('app-drawer-layout partial-panel-resolver');
+  root = root && root.querySelector('app-drawer-layout partial-panel-resolver, ha-drawer partial-panel-resolver');
   root = (root && root.shadowRoot) || root;
   root = root && root.querySelector('ha-panel-lovelace');
   root = root && root.shadowRoot;
